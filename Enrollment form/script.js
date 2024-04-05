bcbb1735121628baa96fe3f5366fb32d5ed11b2a
Ugbabe Anthony Mark <165796112+UgbabeMark@users.noreply.github.com>
2024-04-05 20:37:42 +0200

    Update script.js

--- Enrollment form/script.js
+++ Enrollment form/script.js
@@ -11,7 +11,7 @@ function submitForm() {
     const xhr = new XMLHttpRequest();
 
     // Set up request
-    xhr.open('POST', 'https://55m4km25pg.execute-api.us-east-1.amazonaws.com/prod/register', true);
+    xhr.open('POST', 'API_ARN/enroll', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
 
     // Set up response handler
